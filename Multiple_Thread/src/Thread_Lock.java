8e98a34aaa0cc08732d38bc303cce0c758ae65b6
zxb <zhouxinbing_sbq@163.com>
2022-03-24 20:23:23 +0800

    Thread commit 2

--- Multiple_Thread/src/Thread_Lock.java
+++ Multiple_Thread/src/Thread_Lock.java
@@ -1,33 +1,35 @@
-/**
- * 示例：多窗口卖票   实现Runnable接口方式
- */
-public class Window_Thread_Two {
-    public static void main(String[] args) {
-        Window_ticket_two window_ticket_two = new Window_ticket_two();
-        java.lang.Thread thread = new java.lang.Thread(window_ticket_two);
-        java.lang.Thread thread2 = new java.lang.Thread(window_ticket_two);
-        java.lang.Thread thread3 = new java.lang.Thread(window_ticket_two);
+import java.util.concurrent.locks.ReentrantLock;
 
-        thread.setName("窗口一");
+/**
+ * 同步方式三:   Lock锁
+ */
+public class Thread_Lock {
+    public static void main(String[] args) {
+        Tickets ticket = new Tickets();
+
+        java.lang.Thread thread1 = new java.lang.Thread(ticket);
+        java.lang.Thread thread2 = new java.lang.Thread(ticket);
+        java.lang.Thread thread3 = new java.lang.Thread(ticket);
+
+        thread1.setName("窗口一");
         thread2.setName("窗口二");
         thread3.setName("窗口三");
 
-        thread.start();
+        thread1.start();
         thread2.start();
         thread3.start();
+
     }
 }
 
-class Window_ticket_two implements Runnable {
+class Tickets implements Runnable{
     int tickets = 100;
-    Object object = new Object();
-
+    ReentrantLock lock = new ReentrantLock();
     @Override
     public void run() {
         while (true) {
-            //同步  代码块  解决线程安全问题
-//            synchronized (object) {
-            synchronized (this) {
+            try {
+                lock.lock();
                 if (tickets > 0) {
                     try {
                         java.lang.Thread.sleep(100);
@@ -36,9 +38,11 @@ class Window_ticket_two implements Runnable {
                     }
                     System.out.println(java.lang.Thread.currentThread().getName() + "卖票,票号" + tickets);
                     tickets--;
-                } else {
+                }else {
                     break;
                 }
+            } finally {
+                lock.unlock();
             }
         }
     }
--- Multiple_Thread/src/Window_Thread.java
+++ Multiple_Thread/src/Window_Thread.java
@@ -21,14 +21,26 @@ public class Window_Thread {
 class Window_ticket extends java.lang.Thread {
 
     public static int tickets = 100;  //静态变量  公用
+    static Object object = new Object();
     @Override
     public void run() {
         while (true) {
-            if (tickets>0){
-                System.out.println(getName()+"卖票,票号"+tickets);
-                tickets--;
-            }else {
-                break;
+            //同步  代码块  解决线程安全问题
+//            synchronized (this)   错误   此时对象锁不唯一
+//            synchronized (object) {
+            //Window_ticket.class作为对象 只执行一次
+            synchronized (Window_ticket.class){
+                if (tickets > 0) {
+                    try {
+                        sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(getName() + "卖票,票号" + tickets);
+                    tickets--;
+                } else {
+                    break;
+                }
             }
         }
     }
